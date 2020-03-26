440d2479d979e0f21066892e1d7fe6c036ea5ff9
haphamo <hapham0392@gmail.com>
2020-03-26 16:23:08 -0400

    Added speech bubble and avatar with styling (mobile first)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-
+import avatar from './assets/ha-test-avatar.png'
+import speechBubble from './assets/ha-test-bubble.png'
 import './App.css';
 
 function App() {
   return (
     <div class="container">
-      Hello World
+      <div class="avatar-container">
+        <img id="avatar" alt="chat bot avatar" src={avatar}></img>
+      </div>
+      <div class="speech-container">
+        <img id="speech" alt="speech bubble" src={speechBubble}></img>
+      </div>
     </div>
   );
 }
