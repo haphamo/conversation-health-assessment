817d263ca2320d11f43b27e7475f5e355f8f50a8
haphamo <hapham0392@gmail.com>
2020-03-27 09:34:02 -0400

    The order of methods matter

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React, { useState, useEffect } from 'react'
 import avatar from './assets/ha-test-avatar.png'
 import button from './assets/ha-test-button.png'
 import './App.css';
-import Message from './components/Typewriter'
+import Typewriter from 'typewriter-effect';
+
+// import Message from './components/Typewriter'
 
   
 
 function App() {
+  const [messageCounter, setMessageCounter] = useState(0)
+
+    const nextMessage = function() {
+      setMessageCounter(1)
+    }
 
   return (
     <div>
@@ -14,11 +21,27 @@ function App() {
         <div id="top-section">
           <div id="speech-bubble">
             <div id="text">
-              <Message />
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter.changeDelay(1)
+                  .typeString('Hello World!')
+                  .callFunction(() => {
+                    console.log('String typed out!');
+                    console.log(typewriter);
+                  })
+                  .deleteAll()
+                  .callFunction(() => {
+                    console.log('All strings were deleted');
+                  })
+                  .typeString("I'm here")
+         
+                  .start();
+              }}
+              />
             </div>
           </div>
           <div className="button-and-avatar">
-            <button id="next-button">NEXT</button>
+            <button onClick={nextMessage}id="next-button">NEXT</button>
             <div id="avatar-section">
               <img id="avatar" alt="chat bot avatar" src={avatar}></img>
             </div>
--- src/components/Typewriter.js
+++ src/components/Typewriter.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Typewriter from 'typewriter-effect';
 
 const messages = {
   0:  "Our deep understanding of the Pharma industry ensures we deliver Medical-Legal-Regulatory compliant, friction-free, meaningful customer experiences - while providing compelling business value for our clients.", 
@@ -7,20 +6,9 @@ const messages = {
   2:   "Consumers across all industries are moving to chatbots and voice skills.   Patients and healthcare professionals are no different, but building conversational AI in healthcare is."
 }
 
+export default function Message({ messageCounter }) {
 
-export default function Message(props) {
-    // init typewriter with options
-    const typewriter = new Typewriter('#text', {
-    });
   return (
-    <Typewriter
-      onInit={(typewriter) => {
-        typewriter.typeString(messages[0])
-          .callFunction(() => {
-            console.log('String typed out!');
-          })
-          .start()
-        }}
-      />
+   
   )
 }
