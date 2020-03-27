a9ae1a9f6ff16184c4df68fab62552863b512b51
haphamo <hapham0392@gmail.com>
2020-03-27 00:50:42 -0400

    installed typewriter lib and added component in App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React from 'react'
 import avatar from './assets/ha-test-avatar.png'
-// import speechBubble from './assets/ha-test-bubble.png'
 import button from './assets/ha-test-button.png'
+import Typewriter from 'typewriter-effect';
+
+
 import './App.css';
-// <img id="button" alt="next button for conversation" src={button}></img>
+
+
 function App() {
+
+  const message = new Typewriter('#text', {
+    strings: ['Hello', 'World'],
+    autoStart: true,
+  });
+  console.log(message.context)
   return (
     <div>
 
-      <div class="container">
+      <div className="container">
        
         <div id="top-section">
           <div id="speech-bubble">
             <div id="text">
-              Our deep understanding of the Pharma industry ensures we deliver Medical-Legal-Regulatory compliant, friction-free, meaningful customer experiences - while providing compelling business value for our clients.
+            <Typewriter
+              options={{
+                strings: ['Our deep understanding of the Pharma industry ensures we deliver Medical-Legal-Regulatory compliant, friction-free, meaningful customer experiences - while providing compelling business value for our clients.'],
+                autoStart: true,
+                loop: false,
+              }}
+            />
+              
             </div>
           </div>
-          <div class="button-and-avatar">
+          <div className="button-and-avatar">
             <button id="next-button">NEXT</button>
             <div id="avatar-section">
-            <img id="avatar" alt="chat bot avatar" src={avatar}></img>
-          </div>
+              <img id="avatar" alt="chat bot avatar" src={avatar}></img>
+            </div>
           </div>
           
          
