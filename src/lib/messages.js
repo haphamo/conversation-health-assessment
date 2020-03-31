7215605225a375410be719b9e9cc14e3e8f897e0
haphamo <hapham0392@gmail.com>
2020-03-31 13:14:42 -0400

    created lib dir to contain messages data

---
+++ src/lib/messages.js
@@ -0,0 +1,5 @@
+export const messages = [
+  "<strong>Our deep understanding of the Pharma industry </strong> ensures we deliver Medical-Legal-Regulatory compliant, friction-free, meaningful customer experiences - while providing compelling business value for our clients.",
+  "We are the only conversational AI platform powering healthcare brands.",
+  "<strong>Consumers across all industries are moving to chatbots and voice skills.</strong>   Patients and healthcare professionals are no different, but building conversational AI in healthcare is."
+]
