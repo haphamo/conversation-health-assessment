07e35ea98fccaf4ead310cc6c42928273ef69fa7
haphamo <hapham0392@gmail.com>
2020-03-31 22:59:18 -0400

    Created a function to render all messages into speech bubbles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
-import avatar from "./assets/ha-test-avatar.png";
 
 import "./App.css";
-import Typewriter from "typewriter-effect";
 import { messages } from "./lib/messages"
 import ButtonAndAvatar from "./components/ButtonAndAvatar";
 import Messages from "./components/Messages";
 
 // Next step is to make code DRY
+// The larger the number, the slower the type speed, ex. 500
+const typingSpeed = 5
+
 function App() {
   const [count, setCount] = useState(0);
 
+  // const [ first, ...rest ] = messages
+
   const handleClick = (action) => {
+    // handles creating the speech bubbles
     if(action === "next") {
       setCount(count + 1);
     } else if(action === "back") {
@@ -19,13 +23,21 @@ function App() {
     }
   };
 
+  // const test = messages.map(function(message, index) {
+  //   return `${index}: ${message}`
+  // })
+
+  const messagesToDisplay = messages.map(function(message, index) {
+    return(
+      <Messages key={index} count={count} typingSpeed={typingSpeed} messages={messages[index]}/>
+    )
+  })
+  
   return (
     <div>
       <div className="container">
         <div id="top-section">
-          <div id="speech-bubble">
-            <Messages count={count} messages={messages}/>
-          </div>
+          {messagesToDisplay}
           <ButtonAndAvatar count={count} messages={messages} handleClick={handleClick}/>
         </div>
       </div>
--- src/components/Messages.js
+++ src/components/Messages.js
@@ -4,50 +4,53 @@ import Typewriter from "typewriter-effect";
 // working on separating the typewriter component for more reusability
 export default function Message({ count, messages, typingSpeed}) {
   return (
-    <div id="text">
-      {count === 0 && (
-        <Typewriter
-          onInit={typewriter => {
-            typewriter
-              .pauseFor(1000)
-              .changeDelay(typingSpeed)
-              .typeString(messages)
-              .callFunction(() => {
-                console.log(typewriter);
-              })
-              .start();
-          }}
-        />
-      )}
-      {count === 1 && (
-        <Typewriter
-          onInit={typewriter => {
-            typewriter
-              .changeDelay(20)
-              .typeString(messages)
-              .start();
-          }}
-        />
-      )}
-      {count >= 2 && (
-        <Typewriter
-          onInit={typewriter => {
-            typewriter
-              .changeDelay(20)
-              .typeString(messages)
-              .start();
-          }}
-        />
-      )}
-      {/* <Typewriter
-          onInit={typewriter => {
-            typewriter
-              .pauseFor(1000)
-              .changeDelay(20)
-              .typeString(messages)
-              .start();
-          }}
-        /> */}
+    <div id="speech-bubble">
+      <div id="text">
+        {count === 0 && (
+          <Typewriter
+            onInit={typewriter => {
+              typewriter
+                .pauseFor(1000)
+                .changeDelay(typingSpeed)
+                .typeString(messages)
+                // .callFunction(() => {
+                //   console.log(typewriter);
+                // })
+                .start();
+            }}
+          />
+        )}
+        {count === 1 && (
+          <Typewriter
+            onInit={typewriter => {
+              typewriter
+                .changeDelay(20)
+                .typeString(messages)
+                .start();
+            }}
+          />
+        )}
+        {count >= 2 && (
+          <Typewriter
+            onInit={typewriter => {
+              typewriter
+                .changeDelay(20)
+                .typeString(messages)
+                .start();
+            }}
+          />
+        )}
+        {/* <Typewriter
+            onInit={typewriter => {
+              typewriter
+                .pauseFor(1000)
+                .changeDelay(20)
+                .typeString(messages)
+                .start();
+            }}
+          /> */}
+      </div>
+
     </div>
   );
 }
